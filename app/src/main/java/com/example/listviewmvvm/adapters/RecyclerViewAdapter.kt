01d44a5c0f94ff62b92d92572cab5e47388953a8
Eduardo <Osorio-cruz@hotmail.com>
2019-12-24 20:16:08 -0500

    Implement methods in RecyclerView.
    Signed-off-by: Eduardo <Osorio-cruz@hotmail.com>

--- app/src/main/java/com/example/listviewmvvm/adapters/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/listviewmvvm/adapters/RecyclerViewAdapter.kt
@@ -1,6 +1,9 @@
 package com.example.listviewmvvm.adapters
 
+import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -8,17 +11,22 @@ import android.widget.RelativeLayout
 import android.widget.TextView
 import com.example.listviewmvvm.R
 
-class RecyclerViewAdapter: RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+class RecyclerViewAdapter(var context: Context, var imageNames: ArrayList<String>, var imageViews: ArrayList<String>): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+    private val LOG_TAG = "RecyclerViewAdapter"
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_places, parent, false)
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return imageViews.size
     }
 
-    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        Log.d(LOG_TAG, "onBindViewHolder: called")
+        holder.imageName.text = imageNames[position]
+        holder.imageView.setImageResource(R.drawable.abc_ic_star_black_36dp)
     }
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
