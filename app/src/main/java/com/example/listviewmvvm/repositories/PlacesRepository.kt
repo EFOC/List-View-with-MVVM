772999d16c998227c702e1f0da3916128e58543d
Eduardo <Osorio-cruz@hotmail.com>
2019-12-26 21:49:46 -0500

    Create Singleton class for repository and add placeholder data.
    Signed-off-by: Eduardo <Osorio-cruz@hotmail.com>

--- app/src/main/java/com/example/listviewmvvm/repositories/PlacesRepository.kt
+++ app/src/main/java/com/example/listviewmvvm/repositories/PlacesRepository.kt
@@ -1,13 +1,27 @@
 package com.example.listviewmvvm.repositories
 
-class PlacesRepository {
-    companion object {
-        var instance = PlacesRepository
+import com.example.listviewmvvm.models.Place
+
+object PlacesRepository {
+    lateinit var places: ArrayList<Place>
+
+    fun getPlacesRepo(): ArrayList<Place> {
+        setPlaces()
+        return places
     }
-    fun getInstance(): PlacesRepository.Companion {
-        if(instance == null) {
-            instance = PlacesRepository
-        }
-        return instance
+
+    fun setPlaces() {
+        places = ArrayList()
+        places.add(Place("TEST", "TEST"))
+        places.add(Place("TEST", "TEST"))
+        places.add(Place("TEST", "TEST"))
+        places.add(Place("TEST", "TEST"))
+        places.add(Place("TEST", "TEST"))
+        places.add(Place("TEST", "TEST"))
+        places.add(Place("TEST", "TEST"))
+        places.add(Place("TEST", "TEST"))
+        places.add(Place("TEST", "TEST"))
+        places.add(Place("TEST", "TEST"))
+        places.add(Place("TEST", "TEST"))
     }
 }
